d7b00c57fbf1212531398d0950431f09154461c0
troyamelotte <troyamelotte@gmail.com>
2018-06-18 13:15:50 -0700

    init

--- map.js
+++ map.js
@@ -3,19 +3,25 @@
 let arr = [5, 8, 10];
 
 // What if I wanted to double every element?
-
-for(let i = 0; i<arr.length; i++){
-  arr[i]*=2;
+function doubleAll(arr){
+  for(let i = 0; i<arr.length; i++){
+    arr[i]*=2;
+  }
 }
 
-console.log(arr);
+
+console.log(doubleAll(arr));
 
 // Is there another way?
 
 arr = [5, 8, 10];
 
-let newArr = arr.map((item)=>{
-  return item * 2;
-});
 
-console.log(newArr);
+function doubleMap(arr){
+  return arr.map((item)=>{
+    return item * 2;
+  });
+}
+
+
+console.log(doubleMap(arr));
